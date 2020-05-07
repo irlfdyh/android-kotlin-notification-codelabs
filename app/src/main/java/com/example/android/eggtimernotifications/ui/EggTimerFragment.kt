5b89ef874a174c781ecb10a731b39781a36d2ae2
irshal <irshalfir@gmail.com>
2020-05-08 04:49:37 +0700

    Adding notification channel

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -50,16 +50,38 @@ class EggTimerFragment : Fragment() {
         binding.eggTimerViewModel = viewModel
         binding.lifecycleOwner = this.viewLifecycleOwner
 
-        // TODO: Step 1.7 call create channel
+        // Create notification channel
+        createChannel(
+            getString(R.string.egg_notification_channel_id),
+            getString(R.string.egg_notification_channel_name)
+        )
 
         return binding.root
     }
 
+    /**
+     * Notification channel for android API level 26 above.
+     */
     private fun createChannel(channelId: String, channelName: String) {
-        // TODO: Step 1.6 START create a channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                // Notification importance level
+                NotificationManager.IMPORTANCE_LOW
+            )
 
-        // TODO: Step 1.6 END create a channel
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = "Time for breakfast"
 
+            val notificationManager = requireActivity().getSystemService(
+                NotificationManager::class.java
+            ) as NotificationManager
+
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
     }
 
     companion object {
